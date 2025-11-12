1e1313bbc83788a950417bfafaa0750717975088
sanjidalima <sanjidakhanom245@gmail.com>
2025-11-12 09:28:22 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Managment System</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    
+</head>
+<body>
+    
+    <nav>
+        <level class="logo">Welcome</level>
+
+        <ul>
+            <li><a href ="">Home</a></li>
+            <li><a href ="">Departments</a></li>
+            <li><a href ="">Courses</a></li>
+            <li><a href ="">Contact</a></li>
+            <li><a href ="">Login</a></li>
+    </ul>
+
+    
+    
+</body>
+</html>
