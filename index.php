b7dfbcd9a57a7dee953c24ca600d4d56b485b93c
sanjidalima <sanjidakhanom245@gmail.com>
2025-11-21 18:35:25 +0100

    Refactor index.php for admin login functionality

--- index.php
+++ index.php
@@ -1,27 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>University Managment System</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+<?php
+ob_start ();
+session_start();
+require "php/config.php";
+require_once "php/functions.php";
+$user = new login_registration_class();
+if($user->get_admin_session()){
+	header('Location: admin.php');
+	exit();
+}
+?>
 
-    
-</head>
-<body>
-    
-    <nav>
-        <level class="logo">Welcome</level>
+<?php
+	$pageTitle = "Admin Login";
+?>
+<?php include "header.php"; ?>
 
-        <ul>
-            <li><a href ="">Home</a></li>
-            <li><a href ="">Departments</a></li>
-            <li><a href ="">Courses</a></li>
-            <li><a href ="">Contact</a></li>
-            <li><a href ="">Login</a></li>
-    </ul>
+	<div class="loginform fix">
+		<div class="msg "><h3><i class="fa fa-user" aria-hidden="true"></i>  Admin login</h3></div>
+		<div class="access">
+			<?php
+					if($_SERVER['REQUEST_METHOD'] == "POST"){
+						$username	  = $_POST['username'];
+						$password = $_POST['password'];
 
-    
-    
-</body>
-</html>
+						if(empty($username) or empty($password)){
+							echo "<p style='color:red;text-align:center;'>Field must not be empty.</p>";
+						}else{
+							$password = md5($password);
+							$login = $user->admin_userlogin($username, $password);
+							if($login){
+								header('Location: admin.php');
+							}else{
+								echo "<p style='color:red;text-align:center'>Incorrect username or password</p>";
+							}
+						}
+					}
+				?>
+			<form action="" method="post">
+				<input type="text" name="username" placeholder="Username" />
+				<input type="password" name="password" placeholder="Password" />
+				<input type="submit" value="Login" />
+			</form>
+		</div>
+	</div>
+					
+<?php include "footer.php"; ?>		
