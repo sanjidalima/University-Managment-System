0aee4f85fab26c2bac6e414bc50ad66eedbde579
sanjidalima <sanjidakhanom245@gmail.com>
2025-12-03 09:03:09 +0600

    Update faculty_list.php

--- faculty_list.php
+++ faculty_list.php
@@ -10,22 +10,17 @@ $user = new login_registration_class();
 class PDF extends PDF_Rotate
 {
 	function Header()
-	{
-		//Put the watermark
-		//$this->SetFont('Arial','B',50);
-		//$this->SetTextColor(255,192,203);
-		//$this->RotatedText(65,190,'A p p r o v e d',45);
-	}
+	
 	function RotatedText($x, $y, $txt, $angle)
 	{
-		//Text rotated around its origin
+		
 		$this->Rotate($angle,$x,$y);
 		$this->Text($x,$y,$txt);
 		$this->Rotate(0);
 	}
 }
 
-//$pdf = new FPDF();
+
 $pdf=new PDF();
 $pdf->AddPage();
 
